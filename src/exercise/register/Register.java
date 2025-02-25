cd91a675453a1fff14b7cf93f26d501c5b9ede18
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-02-26 00:12:31 +0100

    add another solution

--- src/exercise/register/Register.java
+++ src/exercise/register/Register.java
@@ -6,18 +6,33 @@ public class Register {
     private Student[] students;
 
     public Register(){
-        this.students = new Student[9];
+        // this.students = new Student[9];
+        this.students = new Student[0];
     };
 
+    // public void addStudent(Student student){
+    //     for (int i = 0; i < students.length; i++) {
+    //         if(students[i] == null){
+    //             students[i]= student;
+    //             break;
+    //         }
+    //     }
+    // }
+
+    //seconda soluzione
     public void addStudent(Student student){
-        for (int i = 0; i < students.length; i++) {
-            if(students[i] == null){
-                students[i]= student;
-                break;
-            }
-        }
+       Student[] newRegister = new Student[this.students.length +1];
+       
+       for (int i = 0; i < this.students.length; i++) {
+            students[i] = newRegister[i];
+       }
+
+       newRegister[newRegister.length-1] = student;
+
+       this.students = newRegister;
     }
 
+    
     public Student[] getStudents(){
         return students;
     }
